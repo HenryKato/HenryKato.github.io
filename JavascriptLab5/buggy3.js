ef5cac434842b618a4eabcd24751442b0525c3f6
Henry Kato <hkato1990@gmail.com>
2021-03-08 21:54:46 +0300

    JS Excercises

---
+++ JavascriptLab5/buggy3.js
@@ -0,0 +1,19 @@
+/*
+
+The function declaration cannot have a semi-colon at the end
+The two single quotes at the end are invalid syntax
+Alert method must be lower case
+
+function gogogo(); {
+
+
+
+    Alert("Yay, it works!'');
+)
+
+*/
+
+function gogogo() {
+    alert("Yay, it works!");
+}
+
---
+++ JavascriptLab5/buggy4.js
@@ -0,0 +1,24 @@
+
+/** 
+ * 
+ * function somethingElse() {
+    if (5 > 2) {             ---> NO CLOSING CURLY BRACKET }
+        alert("Bigger");
+    if (5 < 2) {
+        alert("Smaller");
+    }
+}
+*/
+
+function somethingElse() {
+    if (5 > 2) {
+        alert("Bigger");
+    }
+    if (5 < 2) {
+        alert("Smaller");
+    }
+}
+
+function gogogo() {
+    alert("Yay, it works!");
+}
